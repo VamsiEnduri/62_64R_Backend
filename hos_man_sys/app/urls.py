c76801be78761a769063314ee6452c6718b1e87d
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2026-02-12 17:10:40 +0530

    login + role based dashboard

--- hos_man_sys/app/urls.py
+++ hos_man_sys/app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home,register
+from .views import home,register,login,login_validation,doctorsDashboard,patientsDashboard
 urlpatterns=[
     path("",home),
-    path("register/",register)
+    path("register/",register),
+    path("login/",login),
+    path("login_validation/",login_validation),
+    path("doctorsDashboard/",doctorsDashboard),
+    path("patientsDashboard/",patientsDashboard),
 ]
--- hos_man_sys/app/views.py
+++ hos_man_sys/app/views.py
@@ -1,10 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Doctor,Patient
 # Create your views here.
 def home(req):
     return render(req,"registration.html")
+
+   
+def patientsDashboard(req):
+    c={
+        "name":"ramya"
+    }
+    return render(req,"patientsDashboard.html",c)
+   
+def doctorsDashboard(req):
+    c={
+        "name":"ramya"
+    }
+    return render(req,"DoctorsDashboard.html")
+
+
+@api_view(["POST"])
+def login_validation(req):
+    e=req.data.get("e")
+    p=req.data.get("p")
+    r=req.data.get("r")
+    drs=Doctor.objects.all().values()
+    pts=Patient.objects.all().values()
+    for j in pts:
+        if j["email"] == e and j["password"]  == p:
+            if  r == "Patient" : 
+                return Response({"msg":"login successful patient","r_url":"patientsDashboard"})
+        else:
+            continue
+
+    for i in drs: #10
+        print(type(i))
+        if i["email"] == e and i["password"]  == p:
+            if  r == "Doctor" : 
+                return Response({"msg":"login successful doctor","r_url":"doctorsDashboard"})
+        else:
+            continue
+    return Response({"d":drs})
+
+
+@api_view(["GET"])
+def login(req):
+    return render(req,"login.html")
+
 
 @api_view(["POST"])
 def register(req):
